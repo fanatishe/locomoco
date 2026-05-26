ea4d5c457b41629d382ba65214478cfd8a32484f
Oleh Parshyn <oleh.parshyn@monobank.ua>
2026-05-26 12:53:23 +0300

    added HANDLERS dict

--- assistant/cli/handlers.py
+++ assistant/cli/handlers.py
@@ -3,45 +3,55 @@ from .decorators import input_error
 
 
 @input_error
-def add_contact(args: list[str], book: AddressBook) -> str:
+def add_contact(book: AddressBook, args: list[str]) -> str:
     ...
-
 
 @input_error
-def change_contact(args: list[str], book: AddressBook) -> str:
+def change_contact(book: AddressBook, args: list[str]) -> str:
     ...
-
+    
+@input_error
+def find_contact(book: AddressBook, args: list[str]) -> str:
+    ...
 
 @input_error
-def show_phone(args: list[str], book: AddressBook) -> str:
+def show_phone(book: AddressBook, args: list[str]) -> str:
     ...
-
 
 @input_error
-def show_all(args: list[str], book: AddressBook) -> str:
+def show_all(book: AddressBook, args: list[str]) -> str:
     ...
-
 
 @input_error
-def add_birthday(args: list[str], book: AddressBook) -> str:
+def add_birthday(book: AddressBook, args: list[str]) -> str:
     ...
-
 
 @input_error
-def show_birthday(args: list[str], book: AddressBook) -> str:
+def show_birthday(book: AddressBook, args: list[str]) -> str:
     ...
-
 
 @input_error
-def birthdays(args: list[str], book: AddressBook) -> str:
+def birthdays(book: AddressBook, args: list[str]) -> str:
     ...
-
 
 @input_error
-def delete_contact(args: list[str], book: AddressBook) -> str:
+def delete_contact(book: AddressBook, args: list[str]) -> str:
     ...
-
 
 @input_error
-def fill_mock(args: list[str], book: AddressBook) -> str:
+def fill_mock(book: AddressBook, args: list[str]) -> str:
     ...
+
+
+HANDLERS = {
+    "add": add_contact,
+    "change": change_contact,
+    "delete": delete_contact,
+    "find": find_contact,
+    "phone": show_phone,
+    "all": show_all,
+    "add-birthday": add_birthday,
+    "show-birthday": show_birthday,
+    "birthdays": birthdays,
+    "fill-mock": fill_mock,
+}
