c92d67af2a8617bf322761029ace8973edbd3bbf
iks-ice <olehparshin87@gmail.com>
2026-05-26 12:53:23 +0300

    added HANDLERS dict

--- assistant/cli/runner.py
+++ assistant/cli/runner.py
@@ -2,6 +2,7 @@ from ..domain import AddressBook
 from .decorators import unexpected_exit
 from .parser import parse_input
 from .welcome import welcome
+from .handlers import HANDLERS
 
 @unexpected_exit
 def run(book: AddressBook) -> None:
@@ -11,36 +12,15 @@ def run(book: AddressBook) -> None:
         command, *args = parse_input(user_input)
 
         if command in ["close", "exit"]:
+            print("Bye")
             break
 
         elif command == "hello":
             print("How can I help you?")
 
-        elif command == "add":
-            ...
-
-        elif command == "change":
-            ...
-
-        elif command == "del":
-            ...
-
-        elif command == "phone":
-            ...
-
-        elif command == "all":
-            ...
-
-        elif command == "add-birthday":
-            ...
-
-        elif command == "show-birthday":
-            ...
-
-        elif command == "birthdays":
-            ...
-
-        elif command == "fill-mock":
-            ...
+        elif command in HANDLERS.keys():
+            handler = HANDLERS[command]
+            handler(book, *args)
+        
         else:
             print("Invalid command.")
