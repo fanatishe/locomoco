0b8eb21e240cad1d5c0ef09b44e41fbf91ceb708
fanatishe <anatoly.gapunich@gmail.com>
2026-05-27 15:06:32 +0300

    Store data in a pickle file

--- assistant/cli/interface.py
+++ assistant/cli/interface.py
@@ -1,33 +1,35 @@
-from assistant.contacts.address_book import AddressBook
-
 from assistant.cli.parser import parse_input
 from assistant.cli.commands import COMMANDS
+from assistant.storage.pickle_storage import load_data, save_data
 
 
 def run_cli():
-    book = AddressBook()
+    book = load_data()
 
     print("Welcome to assistant bot!")
 
-    while True:
-        user_input = input("Enter a command: ")
+    try:
+        while True:
+            user_input = input("Enter a command: ")
 
-        command, args = parse_input(user_input)
+            command, args = parse_input(user_input)
 
-        if command in ["close", "exit"]:
-            print("Good bye!")
-            break
+            if command in ["close", "exit"]:
+                print("Good bye!")
+                break
 
-        if command == "hello":
-            print("How can I help you?")
-            continue
+            if command == "hello":
+                print("How can I help you?")
+                continue
 
-        handler = COMMANDS.get(command)
+            handler = COMMANDS.get(command)
 
-        if handler is None:
-            print("Invalid command.")
-            continue
+            if handler is None:
+                print("Invalid command.")
+                continue
 
-        result = handler(args, book)
+            result = handler(args, book)
 
-        print(result)
+            print(result)
+    finally:
+        save_data(book)
