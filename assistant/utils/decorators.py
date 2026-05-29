022b184dc6b85dffc927f84279b19e5ff1c96f65
Oleg Bortovsky <oleg.bortovsky@gmail.com>
2026-05-29 17:33:54 +0300

    fix(decorators): change input_error decortor

--- assistant/utils/decorators.py
+++ assistant/utils/decorators.py
@@ -2,11 +2,13 @@ def input_error(func):
     def inner(*args, **kwargs):
         try:
             return func(*args, **kwargs)
-
+        except IndexError:
+            return "Not enough arguments."
+        except KeyError:
+            return "Contact not found."
         except ValueError as error:
             return str(error)
-
-        except IndexError:
-            return "Enter command arguments."
+        except Exception as error:
+            return f"Error: {error}"
 
     return inner
